181002baa85ba4af62d452d6363f6edf573d1af1
PhuongTramPham <phuongtram150701@gmail.com>
2022-09-18 14:43:34 +0700

    Khoi tao du an, thong tin chi tiet san pham

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import './ui/products/product_detail_screen.dart';
+import './ui/products/products_manager.dart';
+// import 'ui/products/product_overview_screen.dart';
+// import 'ui/products/user_products_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple,).copyWith(
+          secondary: Colors.deepOrange,
+          ),
+      ),
+      home: SafeArea(
+        child: ProductDetailScreen(
+          ProductsManager().items[0],
+        ),
+      ),
+    );
+  }
+}
